1f60814f31abd199cfeaa7fa0af9d5b5cc115135
Amirhossein Rasoulian <ah.rasoulian@gmail.com>
2020-12-15 23:16:18 +0330

    Spliting based on any white space, adding remove_plural_signs and remove_punctuations functions which simplify the tokens.

--- tokenizer.cpp
+++ tokenizer.cpp
@@ -2,7 +2,10 @@
 // /home/amirhossein/Data/University/Fall 2020/Information Retreival/sampleDoc
 Tokenizer::Tokenizer()
 {
+    persian_punctuations << "!" << "،" << "؟" << "." << ":";
 
+    persian_plural_signs << "ها";
+    persian_plural_signs << "ان";
 }
 
 void Tokenizer::find_files(QDir directory){
@@ -32,11 +35,33 @@ void Tokenizer::find_files(QDir directory){
 void Tokenizer::tokenize(QFile *file){
     file->open(QIODevice::ReadOnly | QIODevice::Text);
     QString content = file->readAll();
-    QStringList tokens = content.split(" ");
+    QStringList tokens = content.split(QRegExp("\\s+"));
     file->close();
 
     foreach(QString word, tokens){
-        emit show_message(word);
+        emit show_message(linguistic_modules(word));
     }
+}
+
+QString Tokenizer::linguistic_modules(QString word){
+    word = remove_punctuations(word);
+    word = remove_plural_signs(word);
+    return word;
+}
+
+QString Tokenizer::remove_punctuations(QString word){
+    foreach(QString punctuation, persian_punctuations){
+        if (word.endsWith(punctuation) || word.startsWith(punctuation))
+            word = word.remove(punctuation);
+    }
+    return word;
+}
+
+QString Tokenizer::remove_plural_signs(QString word){
+    foreach(QString sign, persian_plural_signs){
+        if (word.endsWith(sign))
+            word = word.remove(sign);
+    }
+    return word;
 }
 
--- tokenizer.h
+++ tokenizer.h
@@ -13,7 +13,15 @@ public:
     void find_files(QDir directory);
 
 private:
+
     void tokenize(QFile *file);
+
+    QStringList persian_punctuations = {};
+    QStringList persian_plural_signs = {};
+
+    QString linguistic_modules(QString word);
+    QString remove_punctuations(QString word);
+    QString remove_plural_signs(QString word);
 
 signals:
     void show_message(QString message);
